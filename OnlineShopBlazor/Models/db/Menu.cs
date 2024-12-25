19f317eadc1e7fee30860377d5065659879acc20
abbas farahani <abbasfarahani4846@outlook.com>
2024-12-25 14:00:17 +0330

    Add project files.

---
+++ OnlineShopBlazor/Models/db/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShopBlazor.Models.Db;
+
+public partial class Menu
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Link { get; set; }
+
+    public string Type { get; set; } = null!;
+}
---
+++ OnlineShopBlazor/Program.cs
@@ -0,0 +1,35 @@
+﻿using OnlineShopBlazor.Components;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopBlazor.Models.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<OnlineShopContext>();
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
