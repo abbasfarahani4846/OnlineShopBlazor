96f88ab2cb6d7814b6be9cab63bd1c5adb63dbef
abbas farahani <abbasfarahani4846@outlook.com>
2025-04-10 14:14:11 +0330

    register

---
+++ OnlineShopBlazor/Models/Validations/LoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using OnlineShopBlazor.Models.Db;
+
+namespace OnlineShopBlazor.Models.Validations
+{
+
+    public class LoginValidator : AbstractValidator<User>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Please enter your email address.")
+                .EmailAddress().WithMessage("Please enter a valid email address.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Please enter a password.");
+        }
+    }
+}
---
+++ OnlineShopBlazor/Models/Validations/UserValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+using OnlineShopBlazor.Models.Db;
+
+namespace OnlineShopBlazor.Models.Validations
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("Please enter your full name.")
+                .MaximumLength(50).WithMessage("Full name cannot be longer than 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Please enter your email address.")
+                .EmailAddress().WithMessage("Please enter a valid email address.")
+            .MaximumLength(50).WithMessage("Full name cannot be longer than 50 characters.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Please enter a password.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .MaximumLength(50).WithMessage("Full name cannot be longer than 50 characters.");
+        }
+    }
+}
