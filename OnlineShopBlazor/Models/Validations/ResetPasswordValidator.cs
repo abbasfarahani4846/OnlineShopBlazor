8d14beb45742ae9117ca2bcaf63d3441a23c8c13
abbas farahani <abbasfarahani4846@outlook.com>
2025-04-21 11:37:40 +0330

    resset password final

---
+++ OnlineShopBlazor/Models/Validations/ResetPasswordValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using OnlineShopBlazor.Models.ViewModels;
+
+namespace OnlineShopBlazor.Models.Validations
+{
+    public class ResetPasswordValidator : AbstractValidator<ResetPasswordViewModel>
+    {
+        public ResetPasswordValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Please enter your email address.")
+                .EmailAddress().WithMessage("Please enter a valid email address.")
+                .MaximumLength(50).WithMessage("Full name cannot be longer than 50 characters.");
+
+            RuleFor(x => x.RecoveryCode)
+                .NotEmpty().WithMessage("Please enter your recoveryCode address.");
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty().WithMessage("Please enter a password.")
+                .MaximumLength(50).WithMessage("Full name cannot be longer than 50 characters.");
+        }
+    }
+}
---
+++ OnlineShopBlazor/Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopBlazor.Models.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        public string Email { get; set; }
+        public int? RecoveryCode { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
